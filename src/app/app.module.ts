b15564ccfb2ab3ba327c1e74edff2e398758b978
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-11-12 22:16:39 -0500

    ADD: se agregaron servicios rest de manera dinamica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +16,9 @@ import { PanelAdminComponent } from './componentes/panel-admin/panel-admin.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthControllerService } from 'src/app/core/api/services/auth-controller.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  username: FormControl;
+  password: FormControl;
 
-  constructor() { }
+
+  constructor(
+    private service: AuthControllerService,
+  ) {
+    this.username = new FormControl('', [
+      Validators.required,
+      Validators.maxLength(18),
+    ]);
+
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]);
+
+
+    this.form = new FormGroup({
+      username: this.username,
+      password: this.password,
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+
+  submit(): void {
+    this.service
+      .createTokenUsingPOSTResponse({
+        username: this.username.value,
+        password: this.password.value,
+      })
+      .subscribe(
+        (res) => {
+          console.log(res)
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }
