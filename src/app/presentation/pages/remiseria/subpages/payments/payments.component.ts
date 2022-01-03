e089ea247cf98e1846dedf8484328e994f0808e5
Gabriel <gabico_00@hotmail.com>
2022-01-03 15:49:11 -0500

    ADD: Lista de pagos

--- src/app/presentation/pages/remiseria/subpages/payments/payments.component.ts
+++ src/app/presentation/pages/remiseria/subpages/payments/payments.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Payment } from 'src/app/infraestructure/remiseriaApi/models';
+import { PaymentControllerService } from 'src/app/infraestructure/remiseriaApi/services';
 
 @Component({
   selector: 'app-payments',
@@ -8,13 +11,31 @@ import { Payment } from 'src/app/infraestructure/remiseriaApi/models';
 })
 export class PaymentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:PaymentControllerService ) { }
 
   activeModal = false;
   activeUpdated = false;
   currentPayment: Payment = {}
 
+  paymentList : Payment[]=[]
+  dataSource: any = null;
+
+  ocultado = 'd-none';
+  showSpinner = true;
+  displayedColumns: string[] = [
+    'ID',
+    'Driver',
+    'Vehicle',
+    'Employee',
+    'PaymentDate',
+    'Amount',
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    this.loadPaymentsList();
   }
   closeModal(show: boolean): void {
     this.activeModal = show;
@@ -23,12 +44,28 @@ export class PaymentsComponent implements OnInit {
   }
   refreshList(): void {
     // console.log("me ejecuto");
-    // this.ocultado = 'd-none';
-    // this.showSpinner = true;
-    // this.loadRoleList()
+    this.ocultado = 'd-none';
+    this.showSpinner = true;
+    this.loadPaymentsList()
   }
   handlerClickRegister(): void {
     this.activeModal = !this.activeModal;
     this.currentPayment = {};
+  }
+  loadPaymentsList(): void {
+    setTimeout(() => {
+      this.service.getAllUsingGET().subscribe((payments) => {
+        this.paymentList = payments;
+        console.log(payments);
+
+        this.chargingTableList();
+        this.ocultado = payments.length == 0 ? 'd-none' : '';
+        this.showSpinner = false;
+      });
+    }, 2000);
+  }
+  chargingTableList(): void {
+    this.dataSource = new MatTableDataSource<Payment>(this.paymentList);
+    this.dataSource.paginator = this.paginator;
   }
 }
